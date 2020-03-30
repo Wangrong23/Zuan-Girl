7f2e1dc491386fa97fbf87bdd48382d4a6f77b11
gouqi2333 <1902167838@qq.com>
2020-03-30 19:11:03 +0800

    add 祖安语录

--- awesome/plugins/zuan.py
+++ awesome/plugins/zuan.py
@@ -4,15 +4,21 @@ from nonebot import on_command, CommandSession
 
 # on_command 装饰器将函数声明为一个命令处理器
 # 这里 喷 为命令的名字，同时允许使用别名「喷一下」「给我喷」
-@on_command('[CQ:at,qq=2461784356] 喷', aliases=('[CQ:at,qq=2461784356] 喷一下', '[CQ:at,qq=2461784356] 给我喷'))
+@on_command('喷', aliases=('喷一下', '给我喷'))
 async def curse(session: CommandSession):
     # 从会话状态（session.state）中获取成员名称（member），如果当前不存在，则询问用户
     member = session.get('member', prompt='你想喷谁呢？')
     # 获取城市的天气预报
-    curse_report = await get_curse(member)
+    curse_report = await get_curse_at(member)
     # 向用户发送天气预报
     await session.send(curse_report)
 
+
+@on_command('祖安语录', aliases=('祖安话', '来点祖安语录'))
+async def a_curse(session: CommandSession):
+    api_url = 'https://nmsl.shadiao.app/api.php?level=min&lang=zh_cn'
+    res = requests.get(api_url)
+    await session.send(res.text)
 
 # curse.args_parser 装饰器将函数声明为 curse 命令的参数解析器
 # 命令解析器用于将用户输入的参数解析成命令真正需要的数据
@@ -37,7 +43,7 @@ async def _(session: CommandSession):
     # 如果当前正在向用户询问更多信息（例如本例中的要查询的城市），且用户输入有效，则放入会话状态
     session.state[session.current_key] = stripped_arg
 
-async def get_curse(member: str) -> str:
+async def get_curse_at(member: str) -> str:
     api_url = 'https://nmsl.shadiao.app/api.php?level=min&lang=zh_cn'
     res = requests.get(api_url)
     return f'[CQ:at,qq={member}] {res.text}'
