e9acb1f6266a427e3fae67342c06f3203c946185
gouqi2333 <1902167838@qq.com>
2020-03-30 20:43:39 +0800

    test

--- awesome/plugins/tiangou.py
+++ awesome/plugins/tiangou.py
@@ -1,7 +1,9 @@
 import requests
 import re
 from nonebot import on_command, CommandSession
+from nonebot.helpers import render_expression
 
+api_url = 'https://chp.shadiao.app/api.php'
 
 # on_command 装饰器将函数声明为一个命令处理器
 # 这里 舔 为命令的名字，同时允许使用别名「舔一下」「给我舔」
@@ -9,15 +11,19 @@ from nonebot import on_command, CommandSession
 async def lick(session: CommandSession):
     # 从会话状态（session.state）中获取成员名称（member），如果当前不存在，则询问用户
     member = session.get('member', prompt='你想舔谁呢？')
-    # 获取城市的天气预报
-    lick_report = await get_lick(member)
-    # 向用户发送天气预报
-    await session.send(lick_report)
+   
+    text = await requests.get(api_url).text
+    await session.send(
+                render_expression(
+                    ('[CQ:at,qq={at_id}] {text}'),
+                    at_id=member,
+                    text=text
+                )
+            )
 
 @on_command('舔狗语录', aliases=('彩虹屁', '来点舔狗语录' , '来点彩虹屁'))
 async def a_lick(session: CommandSession):
-    api_url = 'https://chp.shadiao.app/api.php'
-    res = requests.get(api_url)
+    res = await requests.get(api_url)
     await session.send(res.text)
 
 # lick.args_parser 装饰器将函数声明为 lick 命令的参数解析器
@@ -31,23 +37,26 @@ async def _(session: CommandSession):
     stripped_arg = session.current_arg_text.strip()
     print('stripped_arg',stripped_arg)
 
-    # 该命令第一次运行（第一次进入命令会话）
-    if stripped_arg:
-        # 第一次运行参数不为空，意味着用户直接将城市名跟在命令名后面，作为参数传入
-        # 例如用户可能发送了：天气 南京
-        # 舔一下 [CQ:at,qq=1301236461]
-        pattern = re.compile('[1-9]([0-9]{5,11})')
-        print(pattern.search(stripped_arg))
-        session.state['member'] = pattern.search(stripped_arg)[0]
-    else:
+    if session.is_first_run:
+        # 该命令第一次运行（第一次进入命令会话）
+        if stripped_arg:
+            # 第一次运行参数不为空，意味着用户直接将城市名跟在命令名后面，作为参数传入
+            # 例如用户可能发送了：天气 南京
+            session.state['member'] = stripped_arg
+        return
+
+    if not stripped_arg:
         # 用户没有发送有效的城市名称（而是发送了空白字符），则提示重新输入
         # 这里 session.pause() 将会发送消息并暂停当前会话（该行后面的代码不会被运行）
         session.pause('你想舔谁呢？')
 
+    # if stripped_arg:
+    #     # 舔一下 [CQ:at,qq=1301236461]
+    #     pattern = re.compile('[1-9]([0-9]{5,11})')
+    #     print(pattern.search(stripped_arg))
+    #     session.state['member'] = pattern.search(stripped_arg)[0]
+    # else:
+    #     session.pause('你想舔谁呢？')
+
     # 如果当前正在向用户询问更多信息（例如本例中的要查询的城市），且用户输入有效，则放入会话状态
     session.state[session.current_key] = stripped_arg
-
-async def get_lick(member: str) -> str:
-    api_url = 'https://chp.shadiao.app/api.php'
-    res = requests.get(api_url)
-    return f'[CQ:at,qq={member}] {res.text}'
