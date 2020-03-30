2fc025b0d598861663b2373e7d92e5a91c4af5a0
gouqi2333 <1902167838@qq.com>
2020-03-30 21:12:49 +0800

    test

--- awesome/plugins/zuan.py
+++ awesome/plugins/zuan.py
@@ -1,28 +1,47 @@
 import requests
 from nonebot import on_command, CommandSession
+from nonebot.helpers import render_expression
+
+high_level_api_url = 'https://nmsl.shadiao.app/api.php?lang=zh_cn'
+min_level_api_url = 'https://nmsl.shadiao.app/api.php?level=min&lang=zh_cn'
 
 
 # on_command 装饰器将函数声明为一个命令处理器
 # 这里 喷 为命令的名字，同时允许使用别名「喷一下」「给我喷」
 @on_command('喷', aliases=('喷一下', '给我喷'))
 async def curse(session: CommandSession):
-    # 从会话状态（session.state）中获取成员名称（member），如果当前不存在，则询问用户
     member = session.get('member', prompt='你想喷谁呢？')
-    # 获取城市的天气预报
-    curse_report = await get_curse_at(member)
-    # 向用户发送天气预报
-    await session.send(curse_report)
+    text = await get_min_level_curse_text()
+    await session.send(
+                render_expression(
+                    ('[CQ:at,qq={at_id}] {text}'),
+                    at_id=member,
+                    text=text
+                )
+            )
+
+@on_command('使劲喷', aliases=('使劲喷一下', '给我使劲喷'))
+async def hard_curse(session: CommandSession):
+    member = session.get('member', prompt='你想喷谁呢？')
+    text = await get_high_level_curse_text()
+    await session.send(
+                render_expression(
+                    ('[CQ:at,qq={at_id}] {text}'),
+                    at_id=member,
+                    text=text
+                )
+            )
 
 
 @on_command('祖安语录', aliases=('祖安话', '来点祖安语录'))
 async def a_curse(session: CommandSession):
-    api_url = 'https://nmsl.shadiao.app/api.php?lang=zh_cn'
-    res = requests.get(api_url)
-    await session.send(res.text)
+    text = await get_high_level_curse_text()
+    await session.send(text)
 
 # curse.args_parser 装饰器将函数声明为 curse 命令的参数解析器
 # 命令解析器用于将用户输入的参数解析成命令真正需要的数据
 @curse.args_parser
+@hard_curse.args_parser
 async def _(session: CommandSession):
     # 去掉消息首尾的空白符
     stripped_arg = session.current_arg_text.strip()
@@ -43,7 +62,10 @@ async def _(session: CommandSession):
     # 如果当前正在向用户询问更多信息（例如本例中的要查询的城市），且用户输入有效，则放入会话状态
     session.state[session.current_key] = stripped_arg
 
-async def get_curse_at(member: str) -> str:
-    api_url = 'https://nmsl.shadiao.app/api.php?level=min&lang=zh_cn'
-    res = requests.get(api_url)
-    return f'[CQ:at,qq={member}] {res.text}'
+async def get_min_level_curse_text():
+    res = requests.get(min_level_api_url)
+    return res.text
+
+async def get_high_level_curse_text():
+    res = requests.get(high_level_api_url)
+    return res.text
