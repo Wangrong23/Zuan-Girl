967a9b9ffced17c6621c3a88bf1156a01122d41e
gouqi2333 <1902167838@qq.com>
2020-03-31 20:22:49 +0800

    test

--- awesome/plugins/tiangou.py
+++ awesome/plugins/tiangou.py
@@ -9,7 +9,7 @@ api_url = 'https://chp.shadiao.app/api.php'
 # on_natural_language 装饰器将函数声明为一个自然语言处理器
 # keywords 表示需要响应的关键词，类型为任意可迭代对象，元素类型为 str
 # 如果不传入 keywords，则响应所有没有被当作命令处理的消息
-@on_natural_language(keywords={'喷'})
+@on_natural_language(keywords={'舔'})
 async def _(session: NLPSession):
     # 去掉消息首尾的空白符
     stripped_msg = session.msg.strip()
@@ -29,7 +29,7 @@ async def _(session: NLPSession):
     stripped_arg = pattern.search(stripped_msg).group(0)
 
     # 返回意图命令，前两个参数必填，分别表示置信度和意图命令名
-    return IntentCommand(90.0, '喷', current_arg=stripped_arg or '')
+    return IntentCommand(90.0, '舔', current_arg=stripped_arg or '')
 
 
 # on_command 装饰器将函数声明为一个命令处理器
