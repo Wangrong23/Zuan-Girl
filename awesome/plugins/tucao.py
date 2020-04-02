13fa182f1a5658101bf4322ad060dbfba2572fa5
gouqi2333 <1902167838@qq.com>
2020-04-02 20:45:57 +0800

    test

--- awesome/plugins/tucao.py
+++ awesome/plugins/tucao.py
@@ -19,6 +19,17 @@ async def _(session: NLPSession):
 
     # 对消息进行分词
     words = posseg.lcut(stripped_msg)
+    for word in words:
+        # 每个元素是一个 pair 对象，包含 word 和 flag 两个属性，分别表示词和词性
+        if word.flag == 'n':
+            # n	普通名词	f	方位名词	s	处所名词	t	时间
+            # nr	人名	ns	地名	nt	机构名	nw	作品名
+            # nz	其他专名	v	普通动词	vd	动副词	vn	名动词
+            # a	形容词	ad	副形词	an	名形词	d	副词
+            # m	数量词	q	量词	r	代词	p	介词
+            # c	连词	u	助词	xc	其他虚词	w	标点符号
+            # PER	人名	LOC	地名	ORG	机构名	TIME	时间
+            await session.send(word.word+reply_words[rand_int], at_sender=True)
+            break
 
-    # 返回意图命令，前两个参数必填，分别表示置信度和意图命令名
-    await session.send(words[0]+reply_words[rand_int], at_sender=True)
+    
